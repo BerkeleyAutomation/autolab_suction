38062749d73446d5bc61192a3bed5583e8158d09
Matthew Matl <matthewcmatl@gmail.com>
2018-07-09 13:54:22 -0700

    Fix missing import

--- autolab_suction/__init__.py
+++ autolab_suction/__init__.py
@@ -1,9 +1,7 @@
 from Vacuum import Vacuum
 from SerialCommInterface import VacuumSerialInterface
-from SuctionHandControl import VacuumServo
 
 __all__ = [
     'Vacuum',
     'VacuumSerialInterface',
-    'VacuumServo'
     ]
